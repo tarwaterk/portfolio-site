63d2c2b6f0b6a35f7bd979b7ef787db62abae896
Kevin Tarwater <tarwaterk@gmail.com>
2017-05-20 21:07:52 -0500

    Continues building up site

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-console.log("This really works");
+var navLinks = document.querySelectorAll(".nav__menu-item a");
+
+navLinks.forEach(function(navItem) {
+	navItem.addEventListener("click", function(event) {
+		event.preventDefault();
+
+		var selectedSection = document.getElementById(this.href.split("#")[1]);
+		var allSections = document.querySelectorAll(".sect");
+
+		allSections.forEach(function(section) {
+			section.setAttribute("style", "opacity:0;visibility:hidden");
+		});
+
+		selectedSection.setAttribute("style", "opacity:1;visibility:visible");
+	});
+});
